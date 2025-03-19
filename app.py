9be36849338f5fdc4ebe1a9d63a17cecb610ff14
rayilas <keerthanaselvarajan4@gmail.com>
2025-03-19 13:20:34 +0530

    upload resume

--- app.py
+++ app.py
@@ -21,4 +21,13 @@ def upload_resume():
     if request.method == "POST":
         if "resume" not in request.files:
             return "Resume not found"
-        file=request.files
+        file=request.files["resume"]
+        if file.filename=="":
+            return "No selected file"
+        if file and allowed_file(file.filename):
+            filepath=os.path.join(app.config["Upload_folder"],file.filename)
+            file.save(filepath)
+            return f"File uploaded successfully: {file.filename}"
+        return render_template("upload.html")
+    if __name__=="__main__":
+        app.run(debug=True)
